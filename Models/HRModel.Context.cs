3106b365c309ca78a696e8a8043de0ccad7d532e
sneden <dinizsneden@gmail.com>
2020-07-21 12:18:37 +0400

    enctest

--- Models/HRModel.Context.cs
+++ Models/HRModel.Context.cs
@@ -66,5 +66,6 @@ namespace HRworks.Models
         public virtual DbSet<Training_Request> Training_Request { get; set; }
         public virtual DbSet<end_of_service> end_of_service { get; set; }
         public virtual DbSet<emp_joi_sta_change> emp_joi_sta_change { get; set; }
+        public virtual DbSet<encriptiontest> encriptiontests { get; set; }
     }
 }
